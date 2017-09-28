15bad19b750fe6538ffd2f96cbece167c8229dc4
Anthony DeGuzman <deguzman110@gmail.com>
2017-09-28 16:38:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ return cart;
 
 function viewCart() {
   for (var i = 0; i < cart.length; i++) {
-    for (var item in cart[i]) {
-      if (cart.length === 0) {
-        console.log "Your shopping cart is empty.";
-      } else if (cart.length === 1) {
-        console.log(`In your cart, you have ${item} at $${cart[i][item]}`);
-      }
+    var item = cart[i];
+    var itemName = Object.keys(item[i]);
+    var itemPrice = item[itemName];
+
+    if (cart.length === 0) {
+      console.log("Your shopping cart is empty.");
+    } else if (cart.length === 1) {
+      console.log(`In your cart, you have ${itemName} at $${itemPrice}.`);
     }
   }
 }
