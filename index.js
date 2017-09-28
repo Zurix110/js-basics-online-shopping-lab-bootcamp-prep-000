9eb7473d0b5dcbe48a3b3505beadb71146781f9d
Anthony DeGuzman <deguzman110@gmail.com>
2017-09-28 16:16:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.");
   } else if (cart.length === 1) {
-    console.log("In your cart, you have " + item + " at" + ".");
+    console.log("In your cart, you have " + items.item + " at" + items.itemPrice ".");
   }
 
 
